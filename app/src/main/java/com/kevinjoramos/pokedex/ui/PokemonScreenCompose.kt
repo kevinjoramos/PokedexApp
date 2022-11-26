9549ab588bcb46c09960d2e92c0ca62f5796cdc7
kevinjoramos <kevin.jo.ramos@gmail.com>
2022-11-26 13:41:14 -0700

    -developed the basic layout of the composables for the pokedex screen

--- app/src/main/java/com/kevinjoramos/pokedex/ui/PokemonScreenCompose.kt
+++ app/src/main/java/com/kevinjoramos/pokedex/ui/PokemonScreenCompose.kt
@@ -8,12 +8,14 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kevinjoramos.pokedex.models.Pokemon
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
@@ -29,15 +31,49 @@ fun PokemonScreenComposable() {
                 .fillMaxSize()
                 .background(Color.Gray)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "Pokedex", style = TextStyle(fontSize = 32.sp))
-                Spacer(modifier = Modifier.height(32.dp))
-            }
+            Text(text = "Pokedex", style = TextStyle(fontSize = 32.sp))
+            Spacer(modifier = Modifier.height(32.dp))
+            PokemonLazyColumn(pokemonList = (1..15).toList())
         }
     }
 }
+
+@Composable
+fun PokemonLazyColumn(pokemonList: List<Int>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(pokemonList.size) { pokemon->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Cyan),
+            ) {
+                EntryCard(pokemon, Modifier.weight(1f))
+                Spacer(modifier = Modifier.width(16.dp))
+                EntryCard(pokemon, Modifier.weight(1f))
+            }
+
+        }
+    }
+}
+
+@Composable
+fun EntryCard(pokemon: Int, modifier: Modifier) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        elevation = 10.dp,
+        modifier = modifier
+    ) {
+        Text(
+            text = pokemon.toString(),
+            style = TextStyle(fontSize = 25.sp),
+            modifier = Modifier.padding(16.dp)
+
+        )
+    }
+}
+
 
 @Composable
 fun Example() {
