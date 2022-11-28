314a8ed6afc055f81a311f3cad4aedc73f8631e0
kevinjoramos <kevin.jo.ramos@gmail.com>
2022-11-28 03:09:11 -0700

    -Continued working on pokemon screen composables.

--- app/src/main/java/com/kevinjoramos/pokedex/ui/PokemonScreenCompose.kt
+++ app/src/main/java/com/kevinjoramos/pokedex/ui/PokemonScreenCompose.kt
@@ -1,6 +1,8 @@
 package com.kevinjoramos.pokedex
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,7 +12,11 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,38 +26,48 @@ import com.kevinjoramos.pokedex.models.Pokemon
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun PokemonScreenComposable() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-    ) {
+    PokemonScreenBackground() {
+        Text(text = "Pokedex", style = TextStyle(fontSize = 32.sp))
+        Spacer(modifier = Modifier.height(32.dp))
+        PokemonLazyColumn(
+            pokemonList = (listOf("Bulbasaur", "Charmander", "Ivysuar", "Venasuar", "Charmeoleon",
+            "Charizard")))
+    }
+
+}
+
+@Composable
+fun PokemonScreenBackground(content: @Composable () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.pokedex_background),
+            contentDescription = "background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillHeight
+        )
         Column(
-            modifier = Modifiergit 
-                .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+            modifier = Modifier
+                .padding(top = 32.dp, start = 30.dp, end = 30.dp)
                 .fillMaxSize()
-                .background(Color.Gray)
         ) {
-            Text(text = "Pokedex", style = TextStyle(fontSize = 32.sp))
-            Spacer(modifier = Modifier.height(32.dp))
-            PokemonLazyColumn(pokemonList = (1..15).toList())
+            content()
         }
     }
 }
 
 @Composable
-fun PokemonLazyColumn(pokemonList: List<Int>) {
+fun PokemonLazyColumn(pokemonList: List<String>) {
     LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         items(pokemonList.size) { pokemon->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.Cyan),
             ) {
-                EntryCard(pokemon, Modifier.weight(1f))
-                Spacer(modifier = Modifier.width(16.dp))
-                EntryCard(pokemon, Modifier.weight(1f))
+                EntryCard(pokemonList[pokemon], Modifier.weight(1f))
+                Spacer(modifier = Modifier.width(10.dp))
+                EntryCard(pokemonList[pokemon], Modifier.weight(1f))
             }
 
         }
@@ -59,41 +75,62 @@ fun PokemonLazyColumn(pokemonList: List<Int>) {
 }
 
 @Composable
-fun EntryCard(pokemon: Int, modifier: Modifier) {
+fun EntryCard(pokemon: String, modifier: Modifier) {
     Card(
         shape = RoundedCornerShape(10.dp),
         elevation = 10.dp,
-        modifier = modifier
+        modifier = modifier,
+        backgroundColor = Color(0xFF49d0b0)
     ) {
-        Text(
-            text = pokemon.toString(),
-            style = TextStyle(fontSize = 25.sp),
-            modifier = Modifier.padding(16.dp)
-
-        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Column() {
+                Text(
+                    text = pokemon,
+                    style = TextStyle(color = Color.White),
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TypePillShapedText(type = "Grass")
+                Spacer(modifier = Modifier.height(6.dp))
+                TypePillShapedText(type = "Poison")
+                Spacer(modifier = Modifier.height(6.dp))
+                TypePillShapedText(type = "Grass")
+            }
+            Column(
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "#001")
+                Image(
+                    painter = painterResource(id = R.drawable.pokedex_background),
+                    contentDescription = "pokemon sprite placeholder",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(RectangleShape)
+                )
+            }
+        }
     }
 }
 
-
 @Composable
-fun Example() {
-    Column(
+fun TypePillShapedText(type: String) {
+    Box(
         modifier = Modifier
-            .background(Color.Green)
-            .fillMaxSize()
+            .background(Color(0xFF62e0ca), RoundedCornerShape(50)),
+        contentAlignment = Alignment.Center,
     ) {
-        Spacer(
+        Text(
+            text = type,
+            style = TextStyle(color = Color.White, fontSize = 10.sp),
+            maxLines = 1,
             modifier = Modifier
-                .fillMaxHeight(.5f)
+                .padding(horizontal = 10.dp, vertical = 1.dp)
         )
-        Card(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
-            elevation = 10.dp,
-            shape = RoundedCornerShape(10.dp)
-        ) {
-
-        }
     }
 }
